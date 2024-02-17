89de660d0b1d6f973501b6b92e090ed3b2128f98
Belgutei <z5458085@ad.unsw.edu.au>
2024-02-18 09:39:01 +1100

    changed BlogTagUpper text color

--- src/components/BlogTagUpper.tsx
+++ src/components/BlogTagUpper.tsx
@@ -1,7 +1,7 @@
 export default function BlogTagUpper({ text }: { text: string }) {
   return (
     <div>
-      <p className="tracking-tight text-gray-50 my-6 text-6xl font-extrabold">
+      <p className="tracking-tight text-gray-50 my-6 text-6xl font-extrabold text-pink-500">
         {text}
       </p>
       <hr />
