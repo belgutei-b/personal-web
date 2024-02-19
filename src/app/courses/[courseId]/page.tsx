914b2e561ffcac1dac438dc9fb7a3cba29a43b21
Belgutei <z5458085@ad.unsw.edu.au>
2024-02-19 17:57:06 +1100

    added react-icon package and github-icon to the navbar. filtered blogs to each week in course page

--- src/app/courses/[courseId]/page.tsx
+++ src/app/courses/[courseId]/page.tsx
@@ -1,19 +1,27 @@
 import { getBlogsMeta } from "@/lib/blogs";
 
-
-export default async function Page({ params }: { params: { courseId: string } }) {
-  const allBlogs = await getBlogsMeta();
-  // const blogs = allBlogs.filter(blog => blog.courseId === params.courseId);
+export default async function Page({
+  params,
+}: {
+  params: { courseId: string };
+}) {
+  const allBlogsMeta = await getBlogsMeta();
+  const blogsWeekMeta: Meta[][] = [];
+  for (let i = 1; i <= 10; i++) {
+    blogsWeekMeta[i] = allBlogsMeta.filter(
+      (blog) => blog.week === i && blog.courseId === params.courseId
+    );
+  }
   return (
     <div className="flex w-full mb-10">
       <div className="border-r pr-8 ">
-        <p className="text-pink-500 text-right text-2xl font-semibold">week-1</p>
+        <p className="text-pink-500 text-right text-2xl font-semibold">
+          week-1
+        </p>
       </div>
       <div className="pl-8">
-        <div className="mb-4">
-          
-        </div>
+        <div className="mb-4"></div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,20 +2,21 @@
 
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
 
 const links = [
   {
     url: "/",
     name: "~/Belgutei",
   },
-  {
-    url: "/projects",
-    name: "~/Projects",
-  },
-  {
-    url: "/photos",
-    name: "~/Photos",
-  },
+  // {
+  //   url: "/projects",
+  //   name: "~/Projects",
+  // },
+  // {
+  //   url: "/photos",
+  //   name: "~/Photos",
+  // },
   {
     url: "/blogs",
     name: "~/Blogs",
@@ -29,19 +30,24 @@ const links = [
 export default function Navbar() {
   const { data: session } = useSession();
 
-  const listItems = links.map((link) => (
-    <li key={link.name} className="inline-block mr-12">
+  const items = links.map((link) => (
+    <div key={link.name} className="inline-block mr-12">
       <Link href={link.url} className="hover:text-blue-600">
         {link.name}
       </Link>
-    </li>
+    </div>
   ));
   return (
-    <div className="text-blue-300 flex navbar-links space-x-8 text-xl mt-6">
-      <ul>
-        {listItems}
+    <div className="text-blue-300 navbar-links space-x-8 text-xl mt-6 flex w-full">
+      <div>
+        {items}
         {session ? <button onClick={() => signOut()}>Logout</button> : <></>}
-      </ul>
+      </div>
+      <div className="flex-1 flex justify-end">
+        <Link href="https://github.com/bebe0323" target="_blank">
+          <FaGithub size="28" />
+        </Link>
+      </div>
     </div>
   );
 }
