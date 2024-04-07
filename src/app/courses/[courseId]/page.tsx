e8a82a9223722c5bccd4222bcfda5470b885f22b
Belgutei <z5458085@ad.unsw.edu.au>
2024-04-07 18:30:24 +1000

    commented courseId page

--- src/app/courses/[courseId]/page.tsx
+++ src/app/courses/[courseId]/page.tsx
@@ -6,28 +6,30 @@ export default async function Page({
 }: {
   params: { courseId: string };
 }) {
-  const allBlogsMeta = await getBlogsMeta();
-  const blogsWeekMeta: Meta[][] = [];
-  const allWeeks = [];
-  for (let i = 10; i >= 1; i--) {
-    blogsWeekMeta[i] = allBlogsMeta.filter(
-      (blog) => blog.week === i && blog.courseId === params.courseId
-    );
-    if (blogsWeekMeta[i].length > 0) {
-      allWeeks.push(
-        <div className="w-full flex flex-col">
-          <div className="pr-8">
-            <p className="text-pink-500 text-3xl font-semibold">week-{i}</p>
-          </div>
-          <hr className="my-3" />
-          <div>
-            {blogsWeekMeta[i].map((blog) => (
-              <BlogTitleTags blog={blog} />
-            ))}
-          </div>
-        </div>
-      );
-    }
-  }
-  return <div className="flex flex-col w-full mt-10">{allWeeks}</div>;
+  // TODO: complete the page
+  return <div></div>;
+  // const allBlogsMeta = await getBlogsMeta();
+  // const blogsWeekMeta: Meta[][] = [];
+  // const allWeeks = [];
+  // for (let i = 10; i >= 1; i--) {
+  //   blogsWeekMeta[i] = allBlogsMeta.filter(
+  //     (blog) => blog.week === i && blog.courseId === params.courseId
+  //   );
+  //   if (blogsWeekMeta[i].length > 0) {
+  //     allWeeks.push(
+  //       <div className="w-full flex flex-col">
+  //         <div className="pr-8">
+  //           <p className="text-pink-500 text-3xl font-semibold">week-{i}</p>
+  //         </div>
+  //         <hr className="my-3" />
+  //         <div>
+  //           {blogsWeekMeta[i].map((blog) => (
+  //             <BlogTitleTags blog={blog} />
+  //           ))}
+  //         </div>
+  //       </div>
+  //     );
+  //   }
+  // }
+  // return <div className="flex flex-col w-full mt-10">{allWeeks}</div>;
 }
