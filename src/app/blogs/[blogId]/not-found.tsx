101ba37c13fe9ebb8a7deb57c1097d6d40266739
Belgutei <z5458085@ad.unsw.edu.au>
2024-06-26 18:48:50 +1000

    removed all files related to blogs

--- src/app/blogs/[blogId]/not-found.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function NotFound() {
-  return (
-    <h1>The requested page does not exist.</h1>
-  )
-}
--- src/components/BlogTagUpper.tsx
+++
@@ -1,9 +0,0 @@
-export default function BlogTagUpper({ text }: { text: string }) {
-  return (
-    <div className="border-b border-gray-400">
-      <p className="tracking-tight text-gray-50 my-6 text-6xl font-extrabold text-pink-500">
-        {text}
-      </p>
-    </div>
-  );
-}
