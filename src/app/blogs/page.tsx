7a05beb043fd8db53c8779c869e77b42886b3f1d
Belgutei <z5458085@ad.unsw.edu.au>
2025-05-18 17:27:43 +1000

    mobile version tags added to blogs page

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,13 +1,29 @@
 import { getBlogsFrontmatter } from "@/lib/mdx";
+import { FrontmatterType } from "@/types/blog.types";
 import Link from "next/link";
+
+function getTags(blogs: FrontmatterType[]) {
+  const allTags = blogs
+    .map((blog) => blog.tags)
+    .flat()
+    .sort();
+  const uniqueTags: string[] = [];
+  for (let tag of allTags) {
+    if (!uniqueTags.includes(tag)) {
+      uniqueTags.push(tag);
+    }
+  }
+  return uniqueTags;
+}
 
 export default async function Page() {
   const allBlogsFrontmatter = await getBlogsFrontmatter();
   if (!allBlogsFrontmatter) {
     return <div>Error! 404.</div>;
   }
+  const tags = getTags(allBlogsFrontmatter);
   return (
-    <div className="">
+    <div className="flex flex-col w-full">
       <div className="flex flex-col items-center">
         <div className="my-10 text-5xl font-bold w-full md:w-4/5 lg:w-2/3 lg:text-7xl">
           All blogs
@@ -22,7 +38,7 @@ export default async function Page() {
               <div className="flex text-xs text-stone-400 pb-1">
                 <div>{frontmatter.date}</div>
                 <div className="pr-0.5 pl-2">•</div>
-                <div className="flex">
+                <div className="flex flex-wrap">
                   {frontmatter.tags.map((tag) => (
                     <Link
                       href={`/tags/${tag}`}
@@ -51,6 +67,17 @@ export default async function Page() {
               <span className="absolute w-px -inset-y-3 left-[-1rem] bg-gradient-to-b from-black via-custom-green to-black opacity-0 group-hover:opacity-100 transition"></span>
             </div>
           ))}
+      </div>
+      <div className="flex flex-wrap space-x-2 overflow-x-auto">
+        {tags.map((tag) => (
+          <Link
+            className="border border-zinc-500 text-stone-400 mb-2 px-3 py-0.5 rounded-2xl text-sm hover:bg-zinc-400 hover:text-zinc-800"
+            key={tag}
+            href={`/tags/${tag}`}
+          >
+            {tag}
+          </Link>
+        ))}
       </div>
     </div>
   );
